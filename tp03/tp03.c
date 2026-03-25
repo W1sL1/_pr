ce75ca8107388ae34d890678da12aba2a7635c94
W1sL1 <prohorprozorov555@gmail.com>
2026-03-25 20:12:07 +0300

    1

--- tp03/tp03.c
+++ tp03/tp03.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define _CRT_SECURE_NO_WARNINGS
 int main() {
    int* a = NULL; int count = 0; int num;
-   //read all numbers 
    while (scanf("%d", &num) == 1) {
        a = (int*)realloc(a, (count + 1) * sizeof(int));
        if (a == NULL) return 1; a[count++] = num;
    }
-   if (count < 2) {free(a); return 1;}
-   //last num is n
+   if (count < 2) {free(a);return 1;}
+   //last numb is N
    int n = a[count - 1];
-   count--; //now count of elements continued fraction
-   if (n >= count) {free(a); return 1;}
+   count--; // now count is count elem of fraction 
+   if (n >= count) {free(a);return 1;}
    long long p_prev2 = 1, p_prev1 = a[0];
    long long q_prev2 = 0, q_prev1 = 1;
    long long p_curr = p_prev1, q_curr = q_prev1;
-   int i; 
-   for (i = 0; i < 10; i++) {
+   for (int i = 1; i <= n; i++) {
        p_curr = a[i] * p_prev1 + p_prev2;
        q_curr = a[i] * q_prev1 + q_prev2;
        p_prev2 = p_prev1; q_prev2 = q_prev1;
        p_prev1 = p_curr; q_prev1 = q_curr;
    }
    printf("%lld %lld\n", p_curr, q_curr);
-   free(a);
-   return 0; 
+   free(a);return 0;
 }
