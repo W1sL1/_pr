b0d16df6f7ddad813e0bce176a3c3512474bb3c9
W1sL1 <prohorprozorov555@gmail.com>
2026-09-14 16:10:20 +0300

    1

---
+++ tp2ex/_clean.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h> //malloc...
+void main(){
+    // int n1=10 , n2=15;
+    // int *a1 = malloc(10*sizeof(int));
+    // a1 = [3,4,5,1,2,9,8,7,6,10];      //[] {}   // a2 = [15,14,13,12,11,3,4,5,1,2,9,8,7,6,10];  
+    // // int *a2 = malloc(15*sizeof(int));
+    // // for(int i=0;i<n1;i++){
+    // //     scanf("%d",a1[i]);
+    // //     printf("%d",a1[i]);
+    // for (int i=0;i<n1;i++){
+    //     printf("%d",a1[i]);
+    // }
+    
+    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+    }
