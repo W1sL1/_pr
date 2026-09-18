80a644413931e52d308fe436d8c3f585f4f1c05f
W1sL1 <prohorprozorov555@gmail.com>
2026-09-18 17:18:16 +0300

    1

--- z/union.c
+++ z/union.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 union un{
-    int i;
-    char ch;
+    char ch1;
+    char ch2;
 };
 void main(){
     union un my;
-    my.i=12;
-    my.ch='A';
-    printf("%d ",my.i);
-    printf("%c",my.ch);
+    my.ch1='A';
+    my.ch2='B';
+    printf("%c %c",my.ch1,my.ch2);
+    char *ptr1=&my.ch1; char *ptr2=&my.ch2;
+    printf("\n%p %p",ptr1,ptr2);
+    union un *pt=&my;
+    printf("\n%p",pt);
 }
